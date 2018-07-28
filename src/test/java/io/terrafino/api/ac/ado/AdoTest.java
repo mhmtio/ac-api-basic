ce41ef18706364428b842be52b1e5bf5679bfb8d
Matthias Hanitzsch-Moonlight <mhm@terrafino.io>
2018-07-28 12:24:10 +0100

    Fixed failing tests

--- src/test/java/io/terrafino/api/ac/ado/AdoTest.java
+++ src/test/java/io/terrafino/api/ac/ado/AdoTest.java
@@ -77,14 +77,14 @@ public class AdoTest {
     public void canSetStaticAttributeAsLong() throws Exception {
         Ado ado = ac.createAdo("ADO.1");
         ado.set("C0#SA002", 1L);
-        assertThat(ado.get("C0#SA002").toString(), is(1L));
+        assertThat(ado.get("C0#SA002").toLong(), is(1L));
     }
 
     @Test
     public void canSetStaticAttributeAsDouble() throws Exception {
         Ado ado = ac.createAdo("ADO.1");
         ado.set("C0#SA003", 1.5);
-        assertThat(ado.get("C0#SA003").toString(), is(1.5));
+        assertThat(ado.get("C0#SA003").toDouble(), is(1.5));
     }
 
 }
